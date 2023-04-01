d7f761f5ff5c60f814ea654e51e8837adfcd2b02
Advait Suresh <advaitsuresh2002@gmail.com>
2023-04-02 02:57:26 +0530

    add event listener

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { type } from "os";
+import { v4 as uuidV4 } from "uuid";
+
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.querySelector<HTMLFormElement>("#new-task-form");
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+
+type Task = {
+    id: string;
+    title: string;
+    completed: boolean;
+    createdAt: Date;
+};
+
+form?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (input?.value == "" || input?.value == null) {
+        return;
+    }
+
+    const newTask: Task = {
+        id: uuidV4(),
+        title: input.value,
+        completed: false,
+        createdAt: new Date(),
+    };
+
+    addListItem(newTask);
+});
+
+function addListItem(task: Task) {}
